04e38a09de46a06c0c81ac9682f65941892b146c
Joshua Higgins <55254230+lifeupj@users.noreply.github.com>
2020-05-06 12:23:13 -0400

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,23 @@
+const discordDatabase = require(`${process.cwd()}/dbhandler`);
+​
+module.exports = {
+	name: 'balance',
+	description: 'states user\'s balance',
+	usage: '(member)',
+	guildOnly: true,
+	cooldown: 5,
+	async execute(message, args) {
+		Roulette = await discordDatabase.getGuildMemberCurrentAmount(JSON.parse(`{"user_id":"${member.id}","guild_id":"${message.guild.id}"}`));
+		if((args[0]==='points'||args[0]==='point')&&message.mentions.members.size===1){
+			const member = message.mentions.members.first();
+			Roulette = await discordDatabase.getGuildMemberCurrentAmount(JSON.parse(`{"user_id":"${member.id}","guild_id":"${message.guild.id}"}`));
+			let points = 0;
+			if(Roulette){
+				points = Roulette.points;
+			}
+			return message.channel.send(`${member.displayName} has ${points} point(s)`);			
+		}else if(args[0]==='points'||args[0]==='point'){
+			return message.channel.send(`${message.member} you have ${points} point(s)`);
+		}
+	}
+}
